aefc98a17dce2108ecc0a7067c496a12990c6cfb
Michaela <michaela.rudolfova@seznam.cz>
2021-10-15 12:16:32 +0200

    PART1 - 1.1 and 1.2

---
+++ part1/src/Part.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+const Part = ({part, exercises}) =>(
+    <p>{part} has {exercises} exercises.</p>
+)
+
+export default Part
---
+++ part1/src/Total.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+const Total = ({totalExercises}) =>  (
+        <p>Total number of exercises is {totalExercises}.</p>
+    )
+
+export default Total
